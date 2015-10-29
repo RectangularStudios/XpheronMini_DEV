6fed53a204dfc905231005e16171cf1c24d6786a
lothvorn <lothvorn@gmail.com>
2015-10-29 17:55:36 +0100

    Render to texture example ready

---
+++ Assets/MyAssets/Scripts/Support/AutoTurn.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoTurn : MonoBehaviour {
+	public float xSpeed = 1f;
+	public float ySpeed = 1f;
+	public float zSpeed = 1f;
+
+	void Update()
+	{
+		transform.Rotate(
+			xSpeed * Time.deltaTime,
+			ySpeed * Time.deltaTime,
+			zSpeed * Time.deltaTime
+			);
+	}
+}
